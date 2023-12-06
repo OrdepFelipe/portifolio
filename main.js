b48a93d0fcb04b8cd342915afdb4df254b632e34
Pedro <pfdsa1@hotmail.com>
2023-12-06 00:03:38 -0300

    primeira versão

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('#carrosel').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+    
+})
